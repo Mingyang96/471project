907b24d589e8545ee193dd7c5ed53694dc2f4391
Bradley <jizhe.li@ucalgary.ca>
2022-04-15 23:22:54 -0600

    Update View_vehicle_info.php

--- View_vehicle_info.php
+++ View_vehicle_info.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,17 +8,18 @@
     <title>Customer</title>
 </head>
 <style>
-    .controler{
+    .controler {
         text-align: center;
         font-size: 3em;
     }
 </style>
+
 <body>
     <?php include 'navbar.php' ?>
     <div class="controler">
         <p>This is page for viewing the info of the selected vehicle and can be ordered</p>
 
-        <form action="View_vehicle_accident.php" method="psot">
+        <form action="View_vehicle_accident.php" method="post">
 
             <table border="1" width="200" height="200" cellspacing="20" align="center">
                 <tr>
@@ -25,21 +27,23 @@
                     <td>Plate_No</td>
                     <td>Status</td>
                     <td>Model</td>
-                    
+
                 </tr>
                 <tr>
                     <td>a</td>
                     <td>b</td>
                     <td>c</td>
                     <td>d</td>
-                    <td><input type="submit" value="View accident record"/></td>
+                    <td><input type="submit" value="View accident record" /></td>
                 </tr>
             </table>
         </form>
 
-        <br><br><form action="Add_to_order_list.php" method="post">
-            <input type="submit" value="Reserve vehicle"/>
+        <br><br>
+        <form action="Add_to_order_list.php" method="post">
+            <input type="submit" value="Reserve vehicle" />
         </form>
     </div>
 </body>
+
 </html>
